d3d2e5872dafdbd1f4ccf618b9c400f03e39d9c6
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-07 21:22:05 +0530

    add info to secrets_sample.py

---
+++ secrets_sample.py
@@ -0,0 +1,4 @@
+# Get your API credentials from https://developer.spotify.com/
+# Alternatively you may also add environment variables
+CLIENT_ID = "YOUR_CLIENT_ID"
+CLIENT_SECRET = "YOUR_CLIENT_SECRET"
