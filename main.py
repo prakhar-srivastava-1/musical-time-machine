470bccc743498ebc80581088d461241241a3f28d
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-07 21:47:18 +0530

    add SpotifyClient object to main - give a call to search_song method for the 100 songs fetched from billboard.com

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from spotify_data import SpotifyClient
 
 # user_date = input("Which year do you want to travel to? Please enter the date in this format YYYY-MM-DD:\n")
 user_date = "2000-08-12"
@@ -17,8 +18,22 @@ soup = BeautifulSoup(webpage, "html.parser")
 song_titles = soup.select(selector="ul li h3#title-of-a-story")
 # song_artists = soup.select(selector="ul li span.c-label")
 
+# get list of songs
+list_of_songs = list()
+for song_title in song_titles:
+    list_of_songs.append((song_title.getText()).strip())
+
+# search songs details on spotify using the above search query
+sc = SpotifyClient()
+for song in list_of_songs:
+    query_string = f"{song} year:2000"
+    try:
+        sc.search_song(query_string)
+    except IndexError:
+        continue
+
+
 # debug
 # print(soup)
-for song_title in song_titles:
-    print((song_title.getText()).strip())
+
 # print(song_artists[0])
