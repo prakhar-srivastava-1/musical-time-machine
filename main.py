131a4394f7a333e8a94fecdd5ed80e54c4f56c2c
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-07 07:22:38 +0530

    remove song.py - Song class not required; scrape webpage for song titles

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-user_date = input("Which year do you want to travel to? Please enter the date in this format YYYY-MM-DD:\n")
-
+# user_date = input("Which year do you want to travel to? Please enter the date in this format YYYY-MM-DD:\n")
+user_date = "2000-08-12"
 # construct url
 url = f"https://www.billboard.com/charts/hot-100/{user_date}"
 
@@ -13,5 +13,12 @@ webpage = response.text
 # make soup
 soup = BeautifulSoup(webpage, "html.parser")
 
+# fetch songs
+song_titles = soup.select(selector="ul li h3#title-of-a-story")
+# song_artists = soup.select(selector="ul li span.c-label")
+
 # debug
-print(soup)
+# print(soup)
+for song_title in song_titles:
+    print((song_title.getText()).strip())
+# print(song_artists[0])
--- song.py
+++
@@ -1,5 +0,0 @@
-class Song(object):
-
-    def __init__(self, song_title, song_artist):
-        self.song_title = song_title
-        self.song_artist = song_artist
