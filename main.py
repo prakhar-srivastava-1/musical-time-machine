7076be8a4c65921ed8529f87ea65182be53332d6
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-07 06:44:39 +0530

    add logic to create soup

--- main.py
+++ main.py
@@ -6,5 +6,12 @@ user_date = input("Which year do you want to travel to? Please enter the date in
 # construct url
 url = f"https://www.billboard.com/charts/hot-100/{user_date}"
 
+# send request and capture response
+response = requests.get(url)
+webpage = response.text
+
+# make soup
+soup = BeautifulSoup(webpage, "html.parser")
+
 # debug
-print(url)
+print(soup)
